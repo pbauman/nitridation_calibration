55f6a705e54e44ba6a9d9b3d21f818fb5c3ea1e2
Paul T. Bauman <ptbauman@gmail.com>
2013-04-19 22:30:17 +0000

    [nitridation]: Small program to manually sample likelihood.

---
+++ code/nitcal/src/apps/likelihood_sampling.C
@@ -0,0 +1,127 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// NitCal - Nitridation Calibration 
+//
+// Copyright (C) 2012-2013 The PECOS Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2 GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+// C++
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+// NitCal
+#include "config.h"
+#include "gamma_cn_sip.h"
+#include "gamma_cn_likelihood.h"
+
+#ifdef HAVE_QUESO
+// QUESO
+#include "uqGslVector.h"
+#include "uqGslMatrix.h"
+#endif // HAVE_QUESO
+
+int main(int argc, char* argv[])
+{
+#ifdef HAVE_QUESO
+  // Check command line count.
+  if( argc < 3 )
+    {
+      // TODO: Need more consistent error handling.
+      std::cerr << "Error: Must specify libMesh and QUESO input files." << std::endl;
+      exit(1); // TODO: something more sophisticated for parallel runs?
+    }
+
+  // libMesh input file should be first argument
+  std::string libMesh_input_filename = argv[1];
+  std::string QUESO_input = argv[2];
+
+  //************************************************
+  // Initialize environments
+  //************************************************
+  MPI_Init(&argc,&argv);
+
+  uqFullEnvironmentClass* env = new uqFullEnvironmentClass(MPI_COMM_WORLD,
+							   QUESO_input.c_str(),
+							   "",
+							   NULL );
+  {
+    NitridationCalibration::GammaCNSIP<uqGslVectorClass,uqGslMatrixClass> sip( env, "multilevel",
+									       argc, argv,
+									       libMesh_input_filename );
+
+    const uqBaseScalarFunctionClass<uqGslVectorClass,uqGslMatrixClass>& raw_likelihood = sip.get_likelihood_func();
+
+    const NitridationCalibration::GammaCNLikelihood<uqGslVectorClass,uqGslMatrixClass>& likelihood = 
+      libmesh_cast_ref<const NitridationCalibration::GammaCNLikelihood<uqGslVectorClass,uqGslMatrixClass>& >( raw_likelihood );
+
+    std::ofstream values;
+    std::ofstream errors;
+
+    double param = 0.01;
+
+    for( unsigned int s = 0; s < 200; s++ )
+      {
+        double likelihood_value = 0.0;
+        likelihood.update_parameters(param);
+
+        try
+          {
+            likelihood_value = likelihood.evaluate_likelihood();
+          }
+        catch(...)
+          {
+            std::cout << "Caught exception for evaluating likelihood for parameter = " << param << std::endl;
+            errors.open( "error_params.dat" << std::ios::app );
+            errors << << std::scientific << std::setprecision(16) << param << std::end;
+            errors.close();
+
+            param += 0.01;
+            continue;
+          }
+
+        values.open( "likelihood_values.dat", std::ios::app );
+        values << std::scientific << std::setprecision(16)
+               << param << " " << likelihood_value << std::endl;
+        values.close();
+        
+        params += 0.01;
+      }
+  }
+  //************************************************
+  // Finalize environments
+  //************************************************
+  delete env;
+
+  MPI_Finalize();
+
+#else
+
+  std::cout << "Must have linked against a valid QUESO installation for this program to run." << std::endl;
+
+#endif //HAVE_QUESO
+
+  return 0;
+}
