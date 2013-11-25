abfe45cfd1d9977a36f07854e88b708751178a11
Paul T. Bauman <ptbauman@gmail.com>
2013-11-25 17:09:02 +0000

    Added header to stash QoI names.

---
+++ code/nitcal/src/qoi/include/qoi_names.h
@@ -0,0 +1,36 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// NitCal - Nitridation Calibration 
+//
+// Copyright (C) 2012-2013 The PECOS Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2 GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef NITCAL_QOI_NAMES_H
+#define NITCAL_QOI_NAMES_H
+
+namespace NitridationCalibration
+{
+
+  const std::string mass_loss               = "MassLoss";
+  const std::string average_N_mole_fraction = "AverageNMoleFraction";
+  const std::string mass_loss_catalytic     = "MassLossCatalytic";
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_QOI_NAMES_H
