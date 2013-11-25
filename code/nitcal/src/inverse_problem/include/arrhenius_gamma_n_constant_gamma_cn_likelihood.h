2c16df3788cbeb1ba560bfe644cc9f6a922a13e0
Paul T. Bauman <ptbauman@gmail.com>
2013-11-25 18:10:47 +0000

    Added Arrhenius gamma N, constant gamma CN likelihood.

---
+++ code/nitcal/src/inverse_problem/include/arrhenius_gamma_n_constant_gamma_cn_likelihood.h
@@ -0,0 +1,46 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#ifndef NITCAL_ARRHENIUS_GAMMA_N_CONSTANT_GAMMA_CN_LIKELIHOOD_H
+#define NITCAL_ARRHENIUS_GAMMA_N_CONSTANT_GAMMA_CN_LIKELIHOOD_H
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+#include "gamma_n_gamma_cn_likelihood_base.h"
+
+namespace NitridationCalibration
+{
+
+  template<class Vec,class Mat>
+  class ArrheniusGammaNConstantGammaCNLikelihood : public GammaNGammaCNLikelihoodBase<Vec,Mat>
+  {
+  public:
+
+    ArrheniusGammaNConstantGammaCNLikelihood( int argc,
+                                             char** argv,
+                                             MPI_Comm mpi_comm,
+                                             const std::string& input_filename,
+                                             const char* prefix, 
+                                             const uqVectorSetClass<Vec,Mat>& domain_set,
+                                             const bool returns_ln );
+
+    virtual ~ArrheniusGammaNConstantGammaCNLikelihood();
+
+    virtual void update_parameters( const std::vector<double>& params ) const;
+
+  protected:
+
+    double _gamma_CN_nom;
+
+    double _gamma0_N_nom;
+    double _Ta_N_nom;
+
+  };
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_HAVE_QUESO
+
+#endif // NITCAL_ARRHENIUS_GAMMA_N_CONSTANT_GAMMA_CN_LIKELIHOOD_H
---
+++ code/nitcal/src/inverse_problem/src/arrhenius_gamma_n_constant_gamma_cn_likelihood.C
@@ -0,0 +1,83 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+// C++
+#include <cmath>
+
+// This class
+#include "arrhenius_gamma_n_constant_gamma_cn_likelihood.h"
+
+// GRINS
+#include "grins/math_constants.h"
+
+// QUESO
+#include "uqGslVector.h"
+#include "uqGslMatrix.h"
+
+namespace NitridationCalibration
+{
+  template<class Vec,class Mat>
+  ArrheniusGammaNConstantGammaCNLikelihood<Vec,Mat>::ArrheniusGammaNConstantGammaCNLikelihood( int argc,
+                                                                                             char** argv,
+                                                                                             MPI_Comm mpi_comm,
+                                                                                             const std::string& input_filename,
+                                                                                             const char* prefix, 
+                                                                                             const uqVectorSetClass<Vec,Mat>& domain_set,
+                                                                                             const bool returns_ln)
+    : GammaNGammaCNLikelihoodBase<Vec,Mat>(argc,argv,mpi_comm,input_filename,prefix,domain_set,returns_ln)
+  {
+    GetPot input( input_filename );
+
+    _gamma_CN_nom = input( "InverseProblem/gamma_CN_nominal_value", 1.0e-3 );
+
+    _gamma0_N_nom = input( "InverseProblem/gamma0_N_nominal_value", 1.0e-3 );
+
+    _Ta_N_nom = input( "InverseProblem/Ta_N_nominal_value", 300.0 );
+
+    return;
+  }
+
+  template<class Vec,class Mat>
+  ArrheniusGammaNConstantGammaCNLikelihood<Vec,Mat>::~ArrheniusGammaNConstantGammaCNLikelihood()
+  {
+    return;
+  }
+
+  template<class Vec,class Mat>
+  void ArrheniusGammaNConstantGammaCNLikelihood<Vec,Mat>::update_parameters( const std::vector<double>& params ) const
+  {
+    if( params.size() != 3 )
+      {
+        std::cerr << "Error: params size should be 2 for ArrheniusGammaNConstantGammaCNLikelihood" << std::endl
+                  << "       Found size = " << params.size() << std::endl;
+        libmesh_error();
+      }
+
+    this->_interface.set_gamma_CN( params[0]*_gamma_CN_nom );
+
+    std::vector<libMesh::Real> gamma_N_params(2);
+    gamma_N_params[0] = params[1]*_gamma0_N_nom;
+    gamma_N_params[1] = params[2]*_Ta_N_nom;
+
+    this->_interface.set_gamma_N_params( gamma_N_params );
+
+    /*
+    if( this->m_env.fullRank() == 0 )
+    {
+      std::cout << "param = " << params[0] << ", gamma = " << params[0]*_gamma_nom << std::endl;
+    }
+    */
+
+    return;
+  }
+
+  // Instantiate GSL version of this class
+  template class ArrheniusGammaNConstantGammaCNLikelihood<uqGslVectorClass,uqGslMatrixClass>;
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_HAVE_QUESO
