565e6d5b6f0695dc1b91cc011a8eaaf4d39737ab
Paul T. Bauman <ptbauman@gmail.com>
2013-11-26 06:16:14 +0000

    Adding constant gamma N constant gamma CN SIP.

---
+++ code/nitcal/src/inverse_problem/include/constant_gamma_n_constant_gamma_cn_sip.h
@@ -0,0 +1,43 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#ifndef NITCAL_CONSTANT_GAMMA_N_CONSTANT_GAMMA_CN_SIP_H
+#define NITCAL_CONSTANT_GAMMA_N_CONSTANT_GAMMA_CN_SIP_H
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+#include "sip_base.h"
+
+class GetPot;
+
+namespace NitridationCalibration
+{
+  template<class Vec,class Mat>
+  class ConstantGammaNConstantGammaCNSIP : public StatisticalInverseProblemBase<Vec,Mat>
+  {
+  public:
+
+    ConstantGammaNConstantGammaCNSIP( uqBaseEnvironmentClass* env,
+                                      const std::string& method,
+                                      int argc,
+                                      char** argv,
+                                      const std::string& input_filename );
+
+    virtual ~ConstantGammaNConstantGammaCNSIP();
+
+  protected:
+
+    void create_param_space();
+    void create_param_domain( const GetPot& input );
+    void create_likelihood( int argc, char** argv, MPI_Comm mpi_comm,
+                            const GetPot& input );
+
+  };
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_HAVE_QUESO
+
+#endif // NITCAL_CONSTANT_GAMMA_N_CONSTANT_GAMMA_CN_SIP_H
---
+++ code/nitcal/src/inverse_problem/src/constant_gamma_n_constant_gamma_cn_sip.C
@@ -0,0 +1,110 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+// This class
+#include "constant_gamma_n_constant_gamma_cn_sip.h"
+
+// NitCal
+#include "constant_gamma_n_constant_gamma_cn_likelihood.h"
+
+// QUESO
+#include "uqGslVector.h"
+#include "uqGslMatrix.h"
+
+// libMesh
+#include "libmesh/getpot.h"
+
+namespace NitridationCalibration
+{
+  template<class Vec,class Mat>
+  ConstantGammaNConstantGammaCNSIP<Vec,Mat>::ConstantGammaNConstantGammaCNSIP( uqBaseEnvironmentClass* env,
+                                                   const std::string& method,
+                                                   int argc,
+                                                   char** argv,
+                                                   const std::string& libMesh_input_filename )
+    : StatisticalInverseProblemBase<Vec,Mat>( env, method )
+  {
+    GetPot input(libMesh_input_filename);
+
+    this->create_param_space();
+    this->create_param_domain(input);
+    this->create_prior();
+    this->create_posterior();
+    this->create_likelihood(argc, argv, env->subComm().Comm(), input);
+
+    this->create_sip();
+    
+    return;
+  }
+
+  template<class Vec,class Mat>
+  ConstantGammaNConstantGammaCNSIP<Vec,Mat>::~ConstantGammaNConstantGammaCNSIP()
+  {
+    return;
+  }
+
+  template<class Vec,class Mat>
+  void ConstantGammaNConstantGammaCNSIP<Vec,Mat>::create_param_space()
+  {
+    const unsigned int n_params = 2;
+
+    this->_param_space = new uqVectorSpaceClass<Vec,Mat>( *(this->_queso_env),
+							  "param_",
+							  n_params,
+							  NULL );
+
+    return;
+  }
+
+  template<class Vec,class Mat>
+  void ConstantGammaNConstantGammaCNSIP<Vec,Mat>::create_param_domain( const GetPot& input )
+  {
+    // These are assuming normalized values
+    const double gamma_CN_min = input("InverseProblem/gamma_CN_min", 0.0);
+    const double gamma_N_min = input("InverseProblem/gamma_N_min", 0.0);
+
+    const double gamma_CN_max = input("InverseProblem/gamma_CN_max", 1000.0);
+    const double gamma_N_max = input("InverseProblem/gamma_N_max", 1000.0);
+
+    Vec param_mins( this->_param_space->zeroVector() );
+    param_mins[0] = gamma_CN_min;
+    param_mins[1] = gamma_N_min;
+
+    Vec param_maxs( this->_param_space->zeroVector() );
+    param_maxs[0] = gamma_CN_max;
+    param_maxs[1] = gamma_N_max;
+
+    this->_param_domain = new uqBoxSubsetClass<Vec,Mat>("param_",
+							*(this->_param_space),
+							param_mins,
+							param_maxs );
+    
+    return;
+  }
+
+  template<class Vec,class Mat>
+  void ConstantGammaNConstantGammaCNSIP<Vec,Mat>::create_likelihood( int argc,
+                                                                     char** argv,
+                                                                     MPI_Comm mpi_comm,
+                                                                     const GetPot& input )
+  {
+    this->_likelihood = 
+      new ConstantGammaNConstantGammaCNLikelihood<Vec,Mat>( argc, argv, mpi_comm,
+                                                            input,
+                                                            "like_",
+                                                            *(this->_param_domain),
+                                                            true ); // the routine computes [ln(function)]
+
+    return;
+  }
+
+  // Instantiate GSL version of this class
+  template class ConstantGammaNConstantGammaCNSIP<uqGslVectorClass,uqGslMatrixClass>;
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_HAVE_QUESO
