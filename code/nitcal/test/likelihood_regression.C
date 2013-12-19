e3d84181eb4370da263d3d15e70801314d1d9fde
Paul T. Bauman <ptbauman@gmail.com>
2013-12-19 00:26:56 +0000

    Fully functioning regression test for single experiment likelhiood (that caught 2 bugs!).

--- code/nitcal/test/likelihood_regression.C
+++ code/nitcal/test/likelihood_regression.C
@@ -38,14 +38,17 @@
 #include "uqGslVector.h"
 #include "uqGslMatrix.h"
 
+// GRINS
+#include "grins/math_constants.h"
+
 int main(int argc, char* argv[])
 {
 
   // Check command line count.
-  if( argc < 3 )
+  if( argc < 4 )
     {
       // TODO: Need more consistent error handling.
-      std::cerr << "Error: Must specify QUESO input file." << std::endl;
+      std::cerr << "Error: Must specify QUESO input file, mass loss value, and average N value" << std::endl;
       exit(1); // TODO: something more sophisticated for parallel runs?
     }
 
@@ -58,6 +61,8 @@ int main(int argc, char* argv[])
 							   "",
 							   NULL );
 
+  int return_flag = 0;
+
   {
     NitridationCalibration::ConstantGammaNConstantGammaCNSIP<uqGslVectorClass,uqGslMatrixClass> sip( env, "multilevel",
                                                                                        argc, argv,
@@ -68,6 +73,66 @@ int main(int argc, char* argv[])
     const NitridationCalibration::ConstantGammaNConstantGammaCNLikelihood<uqGslVectorClass,uqGslMatrixClass>& likelihood = 
       libmesh_cast_ref<const NitridationCalibration::ConstantGammaNConstantGammaCNLikelihood<uqGslVectorClass,uqGslMatrixClass>& >( raw_likelihood );
 
+
+    const double computed_likelihood = likelihood.evaluate_likelihood();
+
+    //std::cout << std::scientific << std::setprecision(16) << "likelihood = " << computed_likelihood << std::endl;
+
+    const GetPot& sip_input = sip.get_sip_input();
+
+    const GetPot& fp_input = sip.get_forward_run_input();
+
+    const double mass_loss_data = fp_input( "MassLossLikelihood/data_value", 0.0 );
+    if( !fp_input.have_variable( "MassLossLikelihood/data_value" ) )
+      {
+        std::cerr << "Error: Missing mass loss data_value in fp_input" << std::endl;
+        return 1;
+      }
+
+    const double mass_loss_sigma = fp_input( "MassLossLikelihood/sigma", 0.0 );
+    if( !fp_input.have_variable( "MassLossLikelihood/sigma" ) )
+      {
+        std::cerr << "Error: Missing mass loss sigma in fp_input" << std::endl;
+        return 1;
+      }
+
+    const double avg_N_data = fp_input( "AverageNLikelihood/data_value", 0.0 );
+    if( !fp_input.have_variable( "AverageNLikelihood/data_value" ) )
+      {
+        std::cerr << "Error: Missing average N data_value in fp_input" << std::endl;
+        return 1;
+      }
+
+    const double avg_N_sigma = fp_input( "AverageNLikelihood/sigma", 0.0 );
+    if( !fp_input.have_variable( "AverageNLikelihood/sigma" ) )
+      {
+        std::cerr << "Error: Missing average N sigma in fp_input" << std::endl;
+        return 1;
+      }
+
+    const double mass_loss_value = atof(argv[2]);
+    const double avg_N_value = atof(argv[3]);
+
+    const double exact_ln_likelihood = std::log( 1.0/(mass_loss_sigma*std::sqrt(2*GRINS::Constants::pi) ) )
+      + -0.5*(mass_loss_value - mass_loss_data)*(mass_loss_value - mass_loss_data)/(mass_loss_sigma*mass_loss_sigma)
+      + std::log( 1.0/(avg_N_sigma*std::sqrt(2*GRINS::Constants::pi)) )
+      + -0.5*(avg_N_value - avg_N_data)*(avg_N_value - avg_N_data)/(avg_N_sigma*avg_N_sigma);
+
+    //std::cout << "exact_ln_likelihood = " << exact_ln_likelihood << std::endl;
+
+    const double tol = 1.0e-10;
+
+    if( std::fabs( (computed_likelihood - exact_ln_likelihood)/exact_ln_likelihood ) > tol )
+      {
+        std::cerr << std::scientific << std::setprecision(16)
+                  << "Error: Tolerance exceeded in computed likelihood" << std::endl
+                  << "       tolerance              = " << tol << std::endl
+                  << "       computed_ln_likelihood = " << computed_likelihood << std::endl
+                  << "       exact_ln_likelihood    = " << exact_ln_likelihood << std::endl;
+
+        return_flag = 1;
+      }
+
   }
   //************************************************
   // Finalize environments
@@ -75,6 +140,8 @@ int main(int argc, char* argv[])
   delete env;
 
   MPI_Finalize();
+
+  return return_flag;
 
 } // main
 
