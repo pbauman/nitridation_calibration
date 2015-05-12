125bdc4f995c7194523b6c29930ef88889bd4e1a
Paul T. Bauman <ptbauman@gmail.com>
2015-05-12 09:05:37 -0400

    Interface to QUESO::InterpolationSurrogateBuilder
    Is generic. Just supply the dataset and model classes. Task now
    is to finish developing all the model classes.

---
+++ code/nitcal/src/inverse_problem/include/model_interpolation_builder.h
@@ -0,0 +1,45 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#ifndef NITCAL_MODEL_INTERPOLATION_BUILDER_H
+#define NITCAL_MODEL_INTERPOLATION_BUILDER_H
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+// NitCal
+#include "model_interface_base.h"
+
+// QUESO
+#include "queso/InterpolationSurrogateBuilder.h"
+
+namespace NitridationCalibration
+{
+
+  template<class Vec,class Mat>
+  class ModelInterpolationBuilder : public QUESO::InterpolationSurrogateBuilder<Vec,Mat>
+  {
+  public:
+
+    ModelInterpolationBuilder( QUESO::InterpolationSurrogateDataSet<Vec,Mat>& data,
+                               ModelInterfaceBase<Vec,Mat>& model );
+
+    virtual ~ModelInterpolationBuilder(){};
+
+    virtual void evaluate_model( const Vec& domainVector,
+                                 std::vector<double>& values );
+
+  private:
+
+    ModelInterfaceBase<Vec,Mat>& _model;
+
+    unsigned int _count;
+
+  };
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_HAVE_QUESO
+
+#endif // NITCAL_MODEL_INTERPOLATION_BUILDER_H
---
+++ code/nitcal/src/inverse_problem/src/model_interpolation_builder.C
@@ -0,0 +1,53 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+// This class
+#include <model_interpolation_builder.h>
+
+// QUESO
+#include <queso/GslVector.h>
+#include <queso/GslMatrix.h>
+
+namespace NitridationCalibration
+{
+  template<class Vec,class Mat>
+  ModelInterpolationBuilder<Vec,Mat>::ModelInterpolationBuilder( QUESO::InterpolationSurrogateDataSet<Vec,Mat>& data,
+                                                                 ModelInterfaceBase<Vec,Mat>& model )
+    : QUESO::InterpolationSurrogateBuilder<Vec,Mat>(data),
+    _model(model),
+    _count(0)
+  {}
+
+  template<class Vec,class Mat>
+  void ModelInterpolationBuilder<Vec,Mat>::evaluate_model( const Vec& domainVector,
+                                                           std::vector<double>& values )
+  {
+    std::cout << "ModelInterpolationBuilder evaluation " << _count << std::endl;
+
+    //********************************************************************
+    // Copy contents of domainVector to std::vector.
+    // Doing this copy so we don't have to template the parameters class.
+    //********************************************************************
+    unsigned int num_params = domainVector.sizeGlobal();
+
+    std::vector<double> param_values( num_params );
+    for(unsigned int i = 0; i < num_params; i++ )
+      {
+	param_values[i] = domainVector[i];
+      }
+
+    _model.solve( param_values, values );
+
+    _count++;
+  }
+
+  // Instantiate GSL version of this class
+  template class ModelInterpolationBuilder<QUESO::GslVector,QUESO::GslMatrix>;
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_HAVE_QUESO
