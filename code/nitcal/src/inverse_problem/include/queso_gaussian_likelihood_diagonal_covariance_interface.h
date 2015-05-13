3253055a6a1baff7c4ffd561d47bbb36ea05ca89
Paul T. Bauman <ptbauman@gmail.com>
2015-05-13 11:51:04 -0400

    Add interface to QUESO Gaussian likelihood

---
+++ code/nitcal/src/inverse_problem/include/queso_gaussian_likelihood_diagonal_covariance_interface.h
@@ -0,0 +1,41 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+#include <queso/GaussianLikelihoodDiagonalCovariance.h>
+
+#ifndef NITCAL_QUESO_GAUSSIAN_LIKELIHOOD_DIAGONAL_COVARIANCE_INTERFACE_H
+#define NITCAL_QUESO_GAUSSIAN_LIKELIHOOD_DIAGONAL_COVARIANCE_INTERFACE_H
+
+namespace NitridationCalibration
+{
+  template<class Vec,class Mat>
+   class QuesoGaussianLikelihoodDiagonalCovarianceInterface :
+    public QUESO::GaussianLikelihoodDiagonalCovariance<Vec,Mat>
+  {
+  public:
+
+    QuesoGaussianLikelihoodDiagonalCovarianceInterface( const char *prefix,
+                                                        const QUESO::VectorSet<Vec,Mat>& domainSet,
+                                                        const Vec& observations,
+                                                        const Vec& covariance );
+
+    virtual ~QuesoGaussianLikelihoodDiagonalCovarianceInterface(){};
+
+    //! Override to compute the constants
+    virtual double lnValue( const Vec& domainVector,
+                            const Vec* domainDirection,
+                            Vec* gradVector,
+                            Mat* hessianMatrix,
+                            Vec* hessianEffect ) const;
+
+  };
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_QUESO_GAUSSIAN_LIKELIHOOD_DIAGONAL_COVARIANCE_INTERFACE_H
+
+#endif // NITCAL_HAVE_QUESO
---
+++ code/nitcal/src/inverse_problem/src/queso_gaussian_likelihood_diagonal_covariance_interface.C
@@ -0,0 +1,67 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+#include "queso_gaussian_likelihood_diagonal_covariance_interface.h"
+
+namespace NitridationCalibration
+{
+
+  template<class Vec,class Mat>
+  QuesoGaussianLikelihoodDiagonalCovarianceInterface<Vec,Mat>::QuesoGaussianLikelihoodDiagonalCovarianceInterface( const char *prefix,
+                                                                                                                   const QUESO::VectorSet<Vec,Mat>& domainSet,
+                                                                                                                   const Vec& observations,
+                                                                                                                   const Vec& covariance)
+    : QUESO::GaussianLikelihoodDiagonalCovariance<Vec,Mat>(prefix,domainSet,observations,covariance)
+  {}
+
+
+  template<class Vec,class Mat>
+  double QuesoGaussianLikelihoodDiagonalCovarianceInterface<Vec,Mat>::
+  lnValue( const Vec& domainVector,
+           const Vec* domainDirection,
+           Vec* gradVector,
+           Mat* hessianMatrix,
+           Vec* hessianEffect ) const
+  {
+    /* Multivariate Gaussian PDF:
+       (2 \pi)^{-k/2} \det{\Sigma}^{-1/2}
+       \exp{ -1/2 (x-\mu)^T \Sigma^{-1} x(-\mu) }
+
+       ==> ln(pdf) = -k/2*ln(2 \pi) -1/2*\det{\Simga}
+                           - 1/2((x-\mu)^T \Sigma^{-1} x(-\mu))
+
+    The base class computes the third term, so we need to compute the
+    constants. */
+
+    // -1/2 || f(m) - d ||^2
+    double ln_value = QUESO::GaussianLikelihoodDiagonalCovariance<Vec,Mat>::lnValue(domainVector,domainDirection,gradVector,hessianMatrix,hessianMatrix);
+
+    // Get the dimension and convert to double
+    double dim = this->m_observations.sizeGlobal();
+
+    const double pi = 3.1415926535897932384626433832795029L;
+    const double log_2_pi = std::log(2.0*pi);
+
+    // Leading pi constant
+    ln_value -= dim/2.0*log_2_pi;
+
+    // Since we have a diagonal covariance, the \det is just the
+    // product of the covariances
+    double det_sigma = 1.0;
+    for( unsigned int i = 0; i < dim; i++ )
+      {
+        queso_assert_greater( this->m_covariance[i], 0.0 );
+        det_sigma *= this->m_covariance[i];
+      }
+
+    ln_value -= 0.5*det_sigma;
+
+    return ln_value;
+  }
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_HAVE_QUESO
