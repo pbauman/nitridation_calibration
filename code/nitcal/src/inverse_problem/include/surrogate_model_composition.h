705274514853c25bb5d0711630f72ac5f0f910b2
Paul T. Bauman <ptbauman@gmail.com>
2015-05-18 10:56:42 -0400

    First draft of SurrogateModelComposition

---
+++ code/nitcal/src/inverse_problem/include/surrogate_model_composition.h
@@ -0,0 +1,79 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+#ifndef NITCAL_SURROGATE_MODEL_COMPOSITION_H
+#define NITCAL_SURROGATE_MODEL_COMPOSITION_H
+
+// C++
+#include <vector>
+
+// Boost
+#include "boost/scoped_ptr.hpp"
+
+// NitCal
+#include "model_interface_base.h"
+
+// Foward declarations
+class GetPot;
+
+namespace QUESO
+{
+  class BaseEnvironment;
+
+  template<class V, class M>
+  class InterpolationSurrogateIOASCII;
+
+  template<class V, class M>
+  class LinearLagrangeInterpolationSurrogate;
+}
+
+namespace NitridationCalibration
+{
+  template<class Vec,class Mat>
+  class SurrogateModelComposition
+  {
+  public:
+
+    SurrogateModelComposition( const QUESO::BaseEnvironment& queso_env,
+                               const GetPot& model_input );
+
+    ~SurrogateModelComposition();
+
+    void compute_values( const Vec& param_values,
+                         Vec& model_output ) const;
+
+    const Vec& get_observations() const
+    { return *(this->_observations.get()); }
+
+    const Vec& get_covariance() const
+    { return *(this->_covariance.get()); }
+
+  protected:
+
+    boost::scoped_ptr<ModelInterfaceBase<Vec,Mat> > _model;
+
+    std::vector<GetPot*> _forward_model_inputs;
+
+    boost::scoped_ptr<Vec> _observations;
+
+    boost::scoped_ptr<Vec> _covariance;
+
+    std::vector<QUESO::InterpolationSurrogateIOASCII<Vec,Mat>*> _interp_io;
+
+    std::vector<QUESO::LinearLagrangeInterpolationSurrogate<Vec,Mat>*> _interp_surrogate;
+
+  private:
+
+    SurrogateModelComposition();
+
+  };
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_SURROGATE_MODEL_LIKELIHOOD_H
+
+#endif // NITCAL_HAVE_QUESO
---
+++ code/nitcal/src/inverse_problem/src/surrogate_model_composition.C
@@ -0,0 +1,116 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+
+#include "nitcal_config.h"
+
+#ifdef NITCAL_HAVE_QUESO
+
+// This class
+#include "surrogate_model_composition.h"
+
+//NitCal
+#include "model_builder.h"
+
+// QUESO
+#include <queso/InterpolationSurrogateIOASCII.h>
+#include <queso/LinearLagrangeInterpolationSurrogate.h>
+#include <queso/GslVector.h>
+#include <queso/GslMatrix.h>
+
+namespace NitridationCalibration
+{
+  template<class Vec,class Mat>
+  SurrogateModelComposition<Vec,Mat>::SurrogateModelComposition(const QUESO::BaseEnvironment& queso_env,
+                                                                const GetPot& model_input)
+    : _model(ModelBuilder<Vec,Mat>::build_model(queso_env,model_input))
+  {
+    const QUESO::FullEnvironment& full_env = dynamic_cast<const QUESO::FullEnvironment&>( queso_env );
+
+    // Grab the datasets we'll be working with
+    unsigned int n_datasets = model_input.vector_variable_size("Likelihood/datasets");
+
+    // Parse dataset names
+    std::vector<std::string> dataset_names(n_datasets);
+    for( unsigned int d = 0; d < n_datasets; d++ )
+        dataset_names[d] = model_input( "Likelihood/datasets", "DIE!", d );
+
+    // Setup forward model inputs
+    _forward_model_inputs.resize(n_datasets,NULL);
+    for( unsigned int d = 0; d < n_datasets; d++ )
+      _forward_model_inputs[d] = new GetPot( dataset_names[d]+".in" );
+
+    // Setup data space, 2 datapoints per dataset
+    unsigned int n_datapoints = 2*n_datasets;
+    QUESO::VectorSpace<Vec,Mat> data_space( queso_env, "data_", n_datapoints, NULL);
+
+    _observations.reset( data_space.newVector() );
+    _covariance.reset( data_space.newVector() );
+
+    // Parse input files for observations, covariance
+    for( unsigned int d = 0; d < n_datasets; d++ )
+      {
+        (*_observations)[2*d]   = (*_forward_model_inputs[d])("MassLossLikelihood/data_value", 0.0);
+        (*_observations)[2*d+1] = (*_forward_model_inputs[d])("AverageNLikelihood/data_value", 0.0);
+
+        (*_covariance)[2*d]   = (*_forward_model_inputs[d])("MassLossLikelihood/sigma", 0.0);
+        (*_covariance)[2*d+1] = (*_forward_model_inputs[d])("AverageNLikelihood/sigma", 0.0);
+      }
+
+    // Now setup IO classes. These will hold the InterpolationSurrodateData we need for evaluation
+    _interp_io.resize( n_datapoints );
+    for( unsigned int d = 0; d < n_datasets; d++ )
+      {
+        std::string mass_loss_filename = dataset_names[d]+"_massloss.dat";
+        std::string avg_N_filename = dataset_names[d]+"_avgN.dat";
+
+        _interp_io[2*d] = new QUESO::InterpolationSurrogateIOASCII<Vec,Mat>;
+        _interp_io[2*d+1] = new QUESO::InterpolationSurrogateIOASCII<Vec,Mat>;
+
+        _interp_io[2*d]->read( mass_loss_filename, full_env, "");
+        _interp_io[2*d+1]->read( avg_N_filename, full_env, "");
+      }
+
+    // Now setup interpolation surrogates
+    _interp_surrogate.resize( n_datapoints );
+    for( unsigned int d = 0; d < n_datasets; d++ )
+      {
+        _interp_surrogate[2*d] =
+          new QUESO::LinearLagrangeInterpolationSurrogate<Vec,Mat>( _interp_io[2*d]->data() );
+
+        _interp_surrogate[2*d+1] =
+          new QUESO::LinearLagrangeInterpolationSurrogate<Vec,Mat>( _interp_io[2*d+1]->data() );
+      }
+  }
+
+  template<class Vec,class Mat>
+  SurrogateModelComposition<Vec,Mat>::~SurrogateModelComposition()
+  {
+    for( std::vector<GetPot*>::iterator it = _forward_model_inputs.begin();
+         it != _forward_model_inputs.end(); ++it )
+      delete *it;
+
+    for( typename std::vector<QUESO::InterpolationSurrogateIOASCII<Vec,Mat>*>::iterator it = _interp_io.begin();
+         it != _interp_io.end(); ++it )
+      delete *it;
+
+    for( typename std::vector<QUESO::LinearLagrangeInterpolationSurrogate<Vec,Mat>*>::iterator it = _interp_surrogate.begin();
+         it != _interp_surrogate.end(); ++it )
+      delete *it;
+  }
+
+  template<class Vec,class Mat>
+  void SurrogateModelComposition<Vec,Mat>::compute_values( const Vec& param_values,
+                                                           Vec& model_output ) const
+  {
+    queso_require_equal_to( _observations->sizeGlobal(), model_output.sizeGlobal() );
+
+    for( unsigned int d = 0; d < model_output.sizeGlobal(); d++ )
+        model_output[d] = _interp_surrogate[d]->evaluate( param_values );
+  }
+
+  // Instantiate GSL version of this class
+  template class SurrogateModelComposition<QUESO::GslVector,QUESO::GslMatrix>;
+
+} // end namespace NitridationCalibration
+
+#endif // NITCAL_HAVE_QUESO
